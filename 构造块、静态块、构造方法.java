c8b0d4c6658deb4ca415d4feb8625b3d48a3b99f
Yubao-wss <43661069+Yubao-wss@users.noreply.github.com>
2018-12-09 20:03:35 +0800

    静态块优先级最高，构造块其次，构造方法最低
    ---start---
    static A 父类静态代码块
    static B 静态代码块
    i'm A class.父类非静态代码块
    Hello A!父类构造方法
    i'm B class.非静态代码块
    Hello B! 构造方法
    i'm A class.父类非静态代码块
    Hello A!父类构造方法
    i'm B class.非静态代码块
    Hello B! 构造方法
    ---end---

---
+++ 构造块、静态块、构造方法.java
@@ -0,0 +1,37 @@
+class A {
+    //构造方法
+    public A(){
+    System.out.println("Hello A!父类构造方法");
+    }
+    //非静态代码块
+    {
+    System.out.println("i'm A class.父类非静态代码块");
+    }
+    //静态代码块
+    static{
+    System.out.println("static A 父类静态代码块");
+    }
+    }
+class B extends A {
+    //构造方法
+    public B(){
+    System.out.println("Hello B! 构造方法");
+    }
+    //非静态代码块
+    {
+    System.out.println("i'm B class.非静态代码块");
+    }
+    //静态代码块
+    static{
+    System.out.println("static B 静态代码块");
+    }
+}
+    
+    public class Day4{
+    public static void main(String[] args) {
+        System.out.println("---start---");
+        new B();
+        new B();
+        System.out.println("---end---");
+        }
+    }
